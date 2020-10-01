2cf8517b24bb05db7e3bf8e0ace7093c65dd1460
LechKuznecov <46899047+LechKuznecov@users.noreply.github.com>
2020-10-01 20:53:51 +0300

    code for table

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ const port = process.env.SERVER_PORT || 3000;
 app.use(bp.json());
 app.use(cors());
 
+connection.connect((err) => {
+  if (err) throw err;
+  connection.query("SHOW TABLES like 'ride'", (err, result) => {
+    if (err) console.log(err);
+    if (result.length === 0) {
+      connection.query(
+        "CREATE TABLE ride (id INT AUTO_INCREMENT PRIMARY KEY, to TEXT, from TEXT, datetime DATETIME, description TEXT, fullname TEXT, phone NUMBER, type BOOLEAN)",
+        (err, result) => {
+          if (err) throw err;
+          console.log("RIDE database created");
+        }
+      );
+    } else {
+      console.log("DB RIDE already exists, connected");
+    }
+  });
+});
+
 app.get("/", (req, res) => {
   res.json("Welcome to PIX-DRIVE");
 });
