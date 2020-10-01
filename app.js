bc79bed42dfb49260a58ca4e88bc145e5dc8e150
kaleginiene <ieva.kaleginiene@gmail.com>
2020-10-01 20:52:48 +0300

    Added mysql config and createconnection code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bp = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,15 @@ const port = process.env.SERVER_PORT || 3000;
 
 app.use(bp.json());
 app.use(cors());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  sslmode: process.env.DB_SSLMODE,
+});
 
 app.get("/", (req, res) => {
   res.json("Welcome to PIX-DRIVE");
