a756afc020da31a6d1521da71c9778222d171250
kaleginiene <ieva.kaleginiene@gmail.com>
2020-10-01 20:14:26 +0300

    added Port variable

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(bp.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("Hello PIXELIUKAI");
+  res.json("Welcome to PIX-DRIVE");
 });
 
-app.listen(process.env.SERVER_PORT, () =>
+app.listen(port, () =>
   console.log("Server is working " + process.env.SERVER_PORT + " port")
 );
