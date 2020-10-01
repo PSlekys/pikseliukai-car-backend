65ad1e802155a0aa94d168c1e4aa08f0f96ee6a8
Ramunas <zamrif@gmail.com>
2020-10-01 20:55:33 +0300

    app/get mysql added

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ require("dotenv").config();
 
 const app = express();
 
-const port = process.env.SERVER_PORT || 3000
+const port = process.env.SERVER_PORT || 3000;
 
 app.use(bp.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json("welcome to Pix-Drive");
+app.get("/driver", (req, res) => {
+  connection.query(`SELECT * FROM ride WHERE type="driver"`, (err, result) =>
+    res.json(result)
+  );
 });
 
-app.listen(port, () =>
-  console.log(`Server is working at + ${port}`)
-);
+app.get("/passenger", (req, res) => {
+  connection.query(`SELECT * FROM ride WHERE type="passenger"`, (err, result) =>
+    res.json(result)
+  );
+});
+
+app.post("/", (req, res) => {
+  connection.query(
+    `INSERT INTO ride (to, from, datetime, description, name, phone, type) VALUES (${req.body.to}','${req.body.from}','${req.body.datetime}','${req.body.description}','${req.body.name}','${req.body.phone}', '${req.body.type}')`,
+    (err, result) => res.json(result)
+  );
+});
+
+app.listen(port, () => console.log(`Server is working at ${port}`));
